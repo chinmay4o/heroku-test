949a9f180d72fda2a4bcd2db7dbd9a0fffeba51c
surve4404 <surve4404@gmail.com>
2021-12-30 23:45:26 +0530

    new4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import { Customers } from "./models/bhima/customerSchema.js";
+import { Users } from "./models/bhima/userSchema.js";
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.get("/", (req, res) => {
   res.send("api is running");
 });
 
+// Get all customers
 app.get("/customers", async (req, res) => {
   try {
     const data = await Customers.find();
@@ -47,6 +49,35 @@ app.get("/customers", async (req, res) => {
     res.send(error);
   }
 });
+
+
+//Login
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(422).send("fill the details first ");
+    }
+    try {
+      const user = await Users.findOne({ email: email });
+      if (user) {
+        const verifyUser = await bcrypt.compare(password, user.password);
+        if (verifyUser) {
+          const token = await jwt.sign({ _id: user._id }, process.env.SECRET);
+          user.tokens = user.tokens.concat({ token: token });
+          await user.save();
+          console.log(token);
+  
+          res.json({user , tokenAll : token});
+        } else {
+          throw new Error("passsword not valid");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(422).send(err);
+    }
+  });
+  
 
 app.listen(process.env.PORT, () =>
   console.log("listening on port " + process.env.PORT)
